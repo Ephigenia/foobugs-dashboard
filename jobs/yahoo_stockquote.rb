9b05f56f0053b90b627779ee1dc1485606d4a965
Marcel Eichner <love@ephigenia.de>
2013-03-25 11:47:53 +0100

    YahooStockQuote change only used if changed

--- jobs/yahoo_stockquote.rb
+++ jobs/yahoo_stockquote.rb
@@ -9,21 +9,26 @@ require 'csv'
 # Config
 # ------
 # configuration
-yahoo_stockquote_symbol = 'APPL'
-yahoo_stockquote_symbol = 'SIE.HA'
+yahoo_stockquote_symbol = 'AAPL'
 
-SCHEDULER.every '5s', :first_in => 0 do |job|
+SCHEDULER.every '30s', :first_in => 0 do |job|
   http = Net::HTTP.new("download.finance.yahoo.com")
   response = http.request(Net::HTTP::Get.new("/d/quotes.csv?fb=ac1&s=#{yahoo_stockquote_symbol}"))
   
   if response.code != "200"
     puts "yahoo stock quote communication error (status-code: #{response.code})\n#{response.body}"
   else
+    # read data from csv
     data = CSV.parse(response.body)
-    value = data[0][0].to_f
+    current = data[0][0].to_f
     change = data[0][1].to_f
-    last = value + change
-      print "value: #{value}   change: #{change}    last: #{last}\n"
-      send_event('yahoo_stock_quote', current: value, last: last)
+    # format data for widget
+    widgetData = {
+      current: current
+    }
+    if change != 0.0
+      widgetData[:last] = current + change
+    end
+    send_event('yahoo_stock_quote', widgetData)
   end
 end
