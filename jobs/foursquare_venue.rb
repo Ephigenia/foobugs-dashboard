21080ff4a7e859e71c12c2c90c0dcd9b5d2bcf59
Marcel Eichner <love@ephigenia.de>
2013-12-19 17:40:20 +0100

    disables ssl cert check in foursquare_venue job

--- jobs/foursquare_venue.rb
+++ jobs/foursquare_venue.rb
@@ -1,5 +1,6 @@
 #!/usr/bin/env ruby
 require 'net/http'
+require 'openssl'
 
 # this job can track the total number of checkins and count of persons that
 # checked in in a foursquare venue without using the foursquare api.
@@ -13,6 +14,8 @@ foursquare_venue_id = ENV['FOURSQUARE_VENUE_ID'] || 'foobugs/4e9834e1c2ee4857b06
 SCHEDULER.every '30s', :first_in => 0 do |job|
   http = Net::HTTP.new("de.foursquare.com", Net::HTTP.https_default_port())
   http.use_ssl = true
+  http.verify_mode = OpenSSL::SSL::VERIFY_NONE # disable ssl certificate check
+
   response = http.request(Net::HTTP::Get.new("/v/#{foursquare_venue_id}"))
   if response.code != "200"
     puts "foursquare communication error (status-code: #{response.code})\n#{response.body}"
