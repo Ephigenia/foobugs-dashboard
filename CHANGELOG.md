0f9294764bb31f7b0f3a6af9b86e7b2fd40d3ec2
Marcel Eichner <love@ephigenia.de>
2013-12-19 17:45:06 +0100

    fixed regular expression in google_play job

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,4 +1,5 @@
 # 2013-12-19
 
 * Fixed bug where SSL Communication was not working in some of the jobs, now SSL Cert Check is disabled by default in the jobs
-* Fixed Regeps in `twitter_user` job for capturing the following, follower and tweet counts
+* Fixed regular expressions in `twitter_user` job for capturing the following, follower and tweet counts
+* Fixed regular expressions in `google_play` job
--- jobs/google_play.rb
+++ jobs/google_play.rb
@@ -1,5 +1,6 @@
 #!/usr/bin/env ruby
 require 'net/http'
+require 'openssl'
 
 # Average+Average Voting on an Android App
 # 
@@ -20,6 +21,7 @@ SCHEDULER.every '10m', :first_in => 0 do |job|
   # prepare request  
   http = Net::HTTP.new("play.google.com", Net::HTTP.https_default_port())
   http.use_ssl = true
+  http.verify_mode = OpenSSL::SSL::VERIFY_NONE # disable ssl certificate check
 
   # scrape detail page of app with appId
   response = http.request(Net::HTTP::Get.new("/store/apps/details?id=#{appId}"))
@@ -27,16 +29,22 @@ SCHEDULER.every '10m', :first_in => 0 do |job|
     puts "google play store website communication (status-code: #{response.code})\n#{response.body}"
   else
     # capture average rating using regexp
-    average_rating = /average-rating-value">([\d,.]+)<\/div>/.match(response.body)
+    average_rating = /class=[\"\']score[\"\']>([\d,.]+)</.match(response.body)
     average_rating = average_rating[1].gsub(",", ".").to_f
-    send_event('google_play_average_rating', current: average_rating)
-    # print "average-rating: #{average_rating}\n"
+    if defined?(send_event)
+      send_event('google_play_average_rating', current: average_rating)
+    else
+      print "average-rating: #{average_rating}\n"
+    end
 
     # capture
-    voters_count = /div class="votes">([\d,.]+)<\/div>/.match(response.body)
+    voters_count = /class=[\"\']reviews-num[\"\']>([\d.]+)</.match(response.body)
     voters_count = voters_count[1].gsub('.', '').to_i
-    send_event('google_play_voters_total', current: voters_count)
-    # print "google_play_voters_total: #{voters_count}\n"
-    
+    if defined?(send_event)
+      send_event('google_play_voters_total', current: voters_count)
+    else
+      print "google_play_voters_total: #{voters_count}\n"
+    end
+
   end
 end
