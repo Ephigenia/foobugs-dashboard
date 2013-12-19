6a3483e0571ee3be57815608ebcb17abd51fdc25
Marcel Eichner <love@ephigenia.de>
2013-12-19 17:22:54 +0100

    disables ssl check in twitter_user job

--- jobs/twitter_user.rb
+++ jobs/twitter_user.rb
@@ -1,5 +1,6 @@
 #!/usr/bin/env ruby
 require 'net/http'
+require 'openssl'
 
 # Track public available information of a twitter user like follower, follower
 # and tweet count by scraping the user profile page.
@@ -11,6 +12,7 @@ twitter_username = ENV['TWITTER_USERNAME'] || 'foobugs'
 SCHEDULER.every '2m', :first_in => 0 do |job|
   http = Net::HTTP.new("twitter.com", Net::HTTP.https_default_port())
   http.use_ssl = true
+  http.verify_mode = OpenSSL::SSL::VERIFY_NONE
   response = http.request(Net::HTTP::Get.new("/#{twitter_username}"))
   if response.code != "200"
     puts "twitter communication error (status-code: #{response.code})\n#{response.body}"
